992ee9b562be2cf5c9636e643c05a9e5bed231da
Nate <nathanieljones96@gmail.com>
2021-02-23 22:25:46 -0800

    Add express to server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function (req, res) {
+	res.send('The server is working');
+});
+
+// process.env.port || required for Heroku
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Server running on port', port));
