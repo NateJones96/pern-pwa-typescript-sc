997e99518c29d4d4ec3be1c33ca76684a70b5928
Nate <nathanieljones96@gmail.com>
2021-02-23 22:33:52 -0800

    Created heroku.js

---
+++ server/heroku.js
@@ -0,0 +1,12 @@
+import { app, express } from './server.js';
+const path = require('path');
+
+// NODE_ENV is set to production on Heroku build
+if (process.env.NODE_ENV === 'production') {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, 'client/build')));
+	// Handle React routing, return all requests to React app
+	app.get('*', function (req, res) {
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
+}
