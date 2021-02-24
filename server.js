a57f02b80fc7fa212a30d06c58808745d6caccf8
Nate <nathanieljones96@gmail.com>
2021-02-24 00:21:35 -0800

    Added api and adjusted package.json scripts for correct build of server and client

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-	res.send('The server is working');
+	res.send('The api is working');
 });
 
 // process.env.port || required for Heroku
---
+++ server/api/Docs.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+const exampleRoute = require('./ExampleRoute.js');
+
+// Use express' Router to 
