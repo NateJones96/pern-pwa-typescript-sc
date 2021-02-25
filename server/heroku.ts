53fcc2bf65c9e0f2a51589e9aab6b1463fffcd96
Nate <nathanieljones96@gmail.com>
2021-02-24 18:41:57 -0800

    Refactor heroku.js to TS

--- server/heroku.ts
+++ server/heroku.ts
@@ -1,13 +1,14 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+import express, { Express, Request, Response } from 'express';
+import path from 'path';
+
+const app: Express = express();
 
 // NODE_ENV is set to production on Heroku build
 if (process.env.NODE_ENV === 'production') {
 	// Serve any static files
 	app.use(express.static(path.join(__dirname, 'client/build')));
 	// Handle React routing, return all requests to React app
-	app.get('*', function (req, res) {
+	app.get('*', function (req: Request, res: Response) {
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
 }
