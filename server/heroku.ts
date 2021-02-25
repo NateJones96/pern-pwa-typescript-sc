811392c800f645a450ef192c07f0b74923e8a3ce
Nate <nathanieljones96@gmail.com>
2021-02-24 18:55:41 -0800

    Changed heroku build path

--- server/heroku.ts
+++ server/heroku.ts
@@ -6,9 +6,9 @@ const app: Express = express();
 // NODE_ENV is set to production on Heroku build
 if (process.env.NODE_ENV === 'production') {
 	// Serve any static files
-	app.use(express.static(path.join(__dirname, 'client/build')));
+	app.use(express.static(path.join(__dirname, 'build')));
 	// Handle React routing, return all requests to React app
 	app.get('*', function (req: Request, res: Response) {
-		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
 	});
 }
