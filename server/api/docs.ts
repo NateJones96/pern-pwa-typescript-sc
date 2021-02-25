1e0b2cfaa9d72969384f693235666e3204d2d539
Nate <nathanieljones96@gmail.com>
2021-02-24 19:26:08 -0800

    Add fake data to api for testing purposes

--- server/api/docs.ts
+++ server/api/docs.ts
@@ -1,12 +1,30 @@
 import express, { Request, Response, Router } from 'express';
 
 const router: Router = express.Router();
-
 const exampleRoute = require('./example-route.ts');
+
+// Example json data. Go to localhost:5000/api/docs to test
+const exampleData = [
+    {
+        id: 1,
+		firstName: 'Nate',
+		lastName: 'Jones',
+	},
+    {
+        id: 2,
+        firstName: 'John',
+        lastName: 'Doe',
+    },
+	{
+		id: 3,
+		firstName: 'Mary',
+		lastName: 'Swanson',
+	}
+];
 
 // Add router binding to /docs, not actual root view
 router.get('/', function (req: Request, res: Response) {
-	res.send('The docs are working');
+	res.send(exampleData);
 });
 
 // Attach /example-route as a child route, along with any children of this route
--- server/api/example-route.ts
+++ server/api/example-route.ts
@@ -2,9 +2,14 @@ import express, { Request, Response, Router } from 'express';
 
 const router: Router = express.Router();
 
+// Example json data. Go to localhost:5000/api/docs to test
+const exampleData = {
+    data: 'Example API Route is working'
+};
+
 // Add binding to /example-route, not actual root view
 router.get('/', function (req: Request, res: Response) {
-	res.send('The example route is working');
+	res.send(exampleData);
 });
 
 
